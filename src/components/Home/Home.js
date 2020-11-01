70263b71114a5f25847c654e59880dfc62f3b5b1
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-10-31 19:09:09 -0500

    initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+        <div>
+            <h1>Ethan Kavanagh</h1>
+            <p>
+                I'm a Full Stack Software Engineer based in Minneapolis, MN who 
+                loves to build creative, unique, and websites and application.
+            </p>
+        </div>
+        );
+    }
+}
+
+export default Home;
