4393c256f9e189b56d69c689e0139fe23dde22b2
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-11-01 17:22:08 -0600

    smooth scrolling effect on click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import {Button} from '@material-ui/core';
+import {scroll} from "react-scroll";
 import './Header.css';
 
 class Header extends Component {
+
+    scrollToBottom() {
+        window.scrollTo(0,document.body.scrollHeight)
+    }
+
     render() {
         return (
             <div className="header">
                 <div className="content">
                     <h1 className="name">ETHAN KAVANAGH</h1>
                     <h2 className="title">Full Stack Software Engineer</h2>
-                    <button className="headerBtn">GET IN TOUCH</button>
+                    <button className="headerBtn" onClick={this.scrollToBottom}>GET IN TOUCH</button>
                 </div>
             </div>
         );
