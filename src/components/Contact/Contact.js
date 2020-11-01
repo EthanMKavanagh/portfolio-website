ce6f9f5a1b9664a6b791e5511c1dccfe89433327
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-11-01 13:33:35 -0600

    initial styling to contact info

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import {Paper, Grid, IconButton} from '@material-ui/core';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import './Contact.css';
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <h2>Location</h2>
-                <p>Minneapolis, MN</p>
+            <Grid 
+                container
+                className="contact"
+                direction="row"
+                justify="center"
+                alignItems="center"
+                spacing={4}
+            >
+                <Grid item>
+                    <Paper elevation={3} className="paper">
+                        <div className="info">
+                            <h2>Location</h2>
+                            <p>Minneapolis, MN</p>
+                        </div>
+                    </Paper>
+                </Grid>
 
-                <h2>Email</h2>
-                <p>ethanmkavanagh@gmail.com</p>
+                <Grid item>
+                    <Paper elevation={3} className="paper">
+                        <div className="info">
+                            <h2>Email</h2>
+                            <p>ethanmkavanagh@gmail.com</p>
+                        </div>
+                    </Paper>
+                </Grid>
 
-                <h2>Phone</h2>
-                <p>(651) 900-0191</p>
+                <Grid item>
+                    <Paper elevation={3} className="paper">
+                        <div className="info">
+                            <h2>Phone</h2>
+                            <p>(651) 900-0191</p>
+                        </div>
+                    </Paper>
+                </Grid>
 
-                <h2>Other</h2>
-                <p><a href="https://github.com/EthanMKavanagh">GitHub</a></p>
-                <p><a href="https://www.linkedin.com/in/ethan-kavanagh-4372311b7/">LinkedIn</a></p>
-            </div>
+                <Grid item>
+                    <Paper elevation={3} className="paper">
+                        <div className="info">
+                            <h2 className="links">Links</h2>
+                            <IconButton>
+                                <a href="https://github.com/EthanMKavanagh">
+                                    <GitHubIcon fontSize="large" />
+                                </a>
+                            </IconButton>
+                            <IconButton>
+                                <a href="https://www.linkedin.com/in/ethan-kavanagh-4372311b7/">
+                                    <LinkedInIcon fontSize="large" />
+                                </a>
+                            </IconButton>
+                        </div>
+                    </Paper>
+                </Grid>
+            </Grid>
         );
     }
 }
